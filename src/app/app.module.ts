63ea74de2ab5c135d78936768f7634286f6a3652
Max TOBELEM <maxtobelem@MacBook-Pro-de-Max.local>
2020-09-10 00:24:58 +0200

    Ajout connexion bdd et page de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,32 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+
+export const firebaseConfig = {
+     apiKey: 'AIzaSyCvMEsfyRk3jQclhSOELwUNZ85tBnlO0VQ',
+     authDomain: 'nardo-8f0cd.firebaseapp.com',
+     databaseURL: 'https://nardo-8f0cd.firebaseio.com',
+     projectId: 'nardo-8f0cd',
+     storageBucket: 'nardo-8f0cd.appspot.com',
+     messagingSenderId: '35818469011',
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
---
+++ src/app/connexion/connexion.page.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { ToastController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-connexion',
+  templateUrl: './connexion.page.html',
+  styleUrls: ['./connexion.page.scss'],
+})
+export class ConnexionPage {
+  dataUser = {
+    email: '',
+    password: ''
+ };
+  connected: boolean;
+  constructor(
+    public toastController: ToastController,
+    public afDB: AngularFireDatabase,
+    public afAuth: AngularFireAuth
+  ) {
+    this.afAuth.authState.subscribe(auth => {
+      if (!auth) {
+        console.log('non connecté');
+        this.connected = false;
+      } else {
+        console.log('connecté: ' + auth.uid);
+        this.connected = true;
+      }
+    });
+  }
+  login() {
+    this.afAuth.signInWithEmailAndPassword(this.dataUser.email, this.dataUser.password);
+    this.dataUser = {
+       email: '',
+       password: ''
+     };
+  }
+  logout() {
+    this.afAuth.signOut();
+  }
+}
